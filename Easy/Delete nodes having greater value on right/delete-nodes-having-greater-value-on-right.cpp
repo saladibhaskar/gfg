7063c11f9de1f856d552ab7e3187ea5a1a5d54c8
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-01-25 23:02:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete nodes having greater value on right/delete-nodes-having-greater-value-on-right.cpp
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+Node * rev(Node * Head)
+{
+    Node *cur=Head;
+    Node * pre=NULL;
+    Node * nxt=NULL;
+    while(cur)
+    {
+        nxt=cur->next;
+        cur->next=pre;
+        pre=cur;
+        cur=nxt;
+    }
+    return pre;
+}
+    
+class Solution
+{
+    public:
+    Node *compute(Node *head)
+    {
+        // your code goes here
+        Node *ans=rev(head);
+        Node *temp=ans;
+        while(temp->next!=NULL)
+        {
+           if(temp->next->data<temp->data)
+           {
+               temp->next=temp->next->next;
+           }
+           else
+           {
+               temp=temp->next;
+           }
+        }
+        Node *fns=rev(ans);
+        return fns;
+        
+        
+        
+        
+    }
+    
+};
+   
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
