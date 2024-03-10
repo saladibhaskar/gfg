9a7a8c57465319a8e9bfdfd3fabc051c8bb2287a
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-03-11 00:13:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int n, int x)
+	{
+	    // Your code goes here
+	    map<int,int>mp;
+	    int i,j,c=0;
+	    for(i=0;i<n;i++)
+	    {
+	        for(j=0;j<n;j++)
+	        {
+	            mp[mat1[i][j]]+=1;
+	        }
+	    }
+	    for(i=0;i<n;i++)
+	    {
+	        for(j=0;j<n;j++)
+	        {
+	            int a=x-mat2[i][j];
+	            if(mp.find(a)!=mp.end() && mp[a]!=-1)
+	            {
+	                c++;
+	                mp[a]-=1;
+	            }
+	        }
+	    }
+	    return c;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+
+        vector<vector<int>> mat1(n, vector<int>(n, -1));
+        vector<vector<int>> mat2(n, vector<int>(n, -1));
+
+        for(int i = 0; i < n; i++)
+        {
+        	for(int j = 0; j < n; j++)
+        	{
+        		cin >> mat1[i][j];
+        	}
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+        	for(int j = 0; j < n; j++)
+        	{
+        		cin >> mat2[i][j];
+        	}
+        }
+
+        Solution ob;
+
+        cout << ob.countPairs(mat1, mat2, n, x) << "\n";
+
+    }
+
+    return 0;
+}
+// } Driver Code Ends
