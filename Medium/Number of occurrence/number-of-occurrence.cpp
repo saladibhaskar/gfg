c54ca91a329a75517eadafa737bb8fef15835a6e
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-03-26 14:44:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    // code here
+	    int i=0,j=n-1,c=0;
+	    while(i<j)
+	    {
+	        if(arr[i]==x)c++;
+	        if(arr[j]==x)c++;
+	        i++;
+	        j--;
+	    }
+	    if(n%2!=0 && arr[n/2]==x)c++;
+	    return c;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
